f666e90f36a8623d63f3cd0fc152cce874de8ff7
Alex <alex@brewingcode.net>
2017-08-10 17:32:52 -0700

    Implement implicit grant flow
    This only gives us access for 1 hour, eventually we will want to do full
    auth code flow, but that involves an API secret, which means it will
    need to be on a server.
    
    https://developer.spotify.com/web-api/authorization-guide/

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template lang="pug">
-  #app
-    h1 Auto Playlist
-    h2 Tracks
-    ul(v-for="t in tracks")
-      li {{t}}
+#app
+  h1 Auto Playlist
+  h2.error(v-if="error") Error: {{error}}
+  h2 Tracks
+  h3
+    span(v-if="authorized") {{authorized}}
+    button(v-else, @click="auth") Authorize
+  ul(v-for="t in tracks")
+    li {{t}}
 </template>
 
 <script lang="coffee">
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import VueLS from 'vue-localstorage'
+
+Vue.use VueResource
+Vue.use VueLS
+
+import Url from 'url'
+import Querystring from 'querystring'
+
+{ log } = console
+
 export default
   name: 'app'
+
   data: ->
     tracks: []
+    authorized: ''
+    error: ''
+    pollTimer: null
+
   mounted: ->
-    console.log 'ready!'
-    @load()
-    @poll = setInterval =>
-      @load()
-    , 2000
+    me = Url.parse window.location.href, true
+    if me.hash
+      params = Querystring.parse me.hash.replace /^#/, ''
+      if params.state is @$localStorage.get('csrf')
+        @$localStorage.set 'access_token', params.access_token
+    else if me.query
+      if me.query.state is @$localStorage.get('csrf')
+        @error = "Spotify authorization error: #{me.query.error}"
+
+    @spotify 'me', (resp) =>
+      @authorized = 'foo'
+      resp.json().then (obj) =>
+        @authorized = "Hello, #{obj.id}"
+      @pollTimer = setInterval =>
+        @poll()
+      , 2000
+
   methods:
-    load: ->
-      console.log 'load!'
+    spotify: (path, cb) ->
+      @$http.get "https://api.spotify.com/v1/#{path}",
+        headers:
+          'Authorization': "Bearer #{@$localStorage.get 'access_token'}"
+      .then cb, (err) ->
+        @error = "Spotify API: #{err.statusText}"
+
+    auth: ->
+      @$localStorage.set 'csrf', 'something'
+
+      url = Url.parse 'https://accounts.spotify.com/authorize'
+      url.query =
+        client_id: 'b07feecc7b624316b5742a3b2593f0e5'
+        response_type: 'token'
+        redirect_uri: window.location.href
+        state: @$localStorage.get 'csrf'
+
+      window.location.href = url.format()
+
+    poll: ->
       @tracks.unshift(new Date())
+
 </script>
 
 <style lang="stylus">
