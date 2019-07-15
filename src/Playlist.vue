d2d702a8374d339a01d22ec5634a876156e44089
Alex <alex@brewingcode.net>
2019-07-15 15:28:53 -0700

    Get API calls for playlists and tracks working
    - Limited to 100 tracks

--- src/Playlist.vue
+++ src/Playlist.vue
@@ -1,11 +1,33 @@
 <template lang="pug">
-  div Edit your playlist {{playlistId}} here!
+  #app.container-fluid.col-lg-6.col-md-9.col-sm-9.col-xs-12(v-if="playlist")
+    h1 Auto Playlist: {{ playlist.name }}
+    h4(v-if="playlist.description") {{ playlist.description }}
+    h3.error(v-if="error") Error: {{error}}
+    table.table.table-striped(v-if="tracks")
+      thead
+      tbody
+        tr(v-for="t in tracklist")
+          td {{t.track.name}}
 </template>
 
 <script lang="coffee">
+import Spotify from './spotify.coffee'
+import _ from 'lodash'
+
 export default
   data: ->
-    params: new URLSearchParams(window.location.search)
+    params: null
+    playlist: null
+    tracks: null
+    error: null
+  mixins: [ Spotify ]
+  mounted: ->
+    @params = new URLSearchParams(window.location.search)
+    id = @params.get 'id'
+    @spotify "playlists/#{id}", null, (resp) => @playlist = resp
+    @spotify "playlists/#{id}/tracks", null, (resp) => @tracks = resp
   computed:
-    playlistId: -> @params.get('id')
+    tracklist: -> _.sortBy @tracks.items, 'added_at'
+
 </script>
+
