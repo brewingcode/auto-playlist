47e17da12b4ad9ecc66434272d7eff2efe02af01
Alex <alex@brewingcode.net>
2019-07-16 10:43:32 -0700

    Keep selected row in viewport while scrolling up and down

--- src/Playlist.vue
+++ src/Playlist.vue
@@ -42,15 +42,32 @@ export default
       if e.code in ['KeyJ', 'ArrowDown']
         @current++
         if @current > @tracks.length - 1
+          # wrapped around the bottom, scroll to very top of page
           @current = 0
-          document.querySelector('tr:first-child').scrollIntoView()
+          document.querySelector('body').scrollIntoView()
+        else
+          # check if row is below viewport, and scroll if needed
+          el = document.querySelector("tr:nth-child(#{@current + 1})")
+          h = window.innerHeight or document.documentElement.clientHeight
+          if el?.getBoundingClientRect().bottom > h
+            el.scrollIntoView(false)
+
       else if e.code in ['KeyK', 'ArrowUp']
         @current--
         if @current < 0
+          # wrapped around the top, scroll to bottom
           @current = @tracks.length - 1
           document.querySelector('tr:last-child').scrollIntoView()
+        else
+          # check if the row is above viewport, and scroll if needed
+          el = document.querySelector("tr:nth-child(#{@current + 1})")
+          h = window.innerHeight or document.documentElement.clientHeight
+          if el?.getBoundingClientRect().top < 0
+            el.scrollIntoView()
+
       @setCurrent()
 
+    # tell Vue about the current row
     setCurrent: ->
       @tracks.forEach (t, i) =>
         if i is @current
