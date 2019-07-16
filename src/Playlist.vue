751a3862c69995522901f8029c18318e52ae5111
Alex <alex@brewingcode.net>
2019-07-16 15:22:14 -0700

    Minor polish and cleanup
    - Add more columns to the playlist table
    
    - Add a list of the keybinds
    
    - Add `left`/`h` and a link to go back home

--- src/Playlist.vue
+++ src/Playlist.vue
@@ -1,14 +1,35 @@
 <template lang="pug">
-  #app.container-fluid.col-lg-6.col-md-9.col-sm-9.col-xs-12
+  #app.container-fluid
+    div
+      a(href="/") back to home
+      h3 Controls (ala vim)
+      table.table-striped.controls
+        thead
+          tr
+            th Key(s)
+            th.text-center Action
+        tbody
+          tr(v-for="c in controls")
+            td(style="font-family:monospace; padding-right:3ex;") {{c[0]}}
+            td.text-center {{c[1]}}
+
     h3.error(v-if="error") Error: {{error}}
     div(v-if="playlist")
       h1 Auto Playlist: {{ playlist.name }}
       h4(v-if="playlist.description") {{ playlist.description }}
-      table.table.table-striped(v-if="tracks.length")
+      table.table.table-striped.table-condensed(v-if="tracks.length")
         thead
+          tr
+            th.text-right(style="padding-right:2ex;") #
+            th Title
+            th Album
+            th Spotify ID
         tbody
           tr(v-for="t,i in tracks" v-bind:class="t.selected ? 'success' : ''")
-            td {{i+1}}. {{t.track.name}}
+            td.text-right(style="padding-right:2ex;") {{i+1}}
+            td.text-left {{t.track.name}}
+            td.text-left {{t.track.album.name}}
+            td(style="font-family:monospace") {{t.track.id}}
 </template>
 
 <script lang="coffee">
@@ -24,6 +45,15 @@ export default
     tracks: []
     error: null
     current: 0
+    controls: [
+      [ 'j/J/down', 'next row/down one row' ]
+      [ 'k/K/up', 'prev row/up one row' ]
+      [ 'l/L/right', 'play track, or fast-forward if track is already playing' ]
+      [ 'h/H/left', 'go back home' ]
+      [ 'g', 'first row' ]
+      [ 'G', 'last row' ]
+      [ 'x/X', 'delete track from playlist' ]
+    ]
   mixins: [ Spotify, Scroll ]
   mounted: ->
     @params = new URLSearchParams(window.location.search)
@@ -61,7 +91,7 @@ export default
         , =>
           @tracks.splice _.findIndex(@tracks, (t) -> t.track.uri is dead), 1
           @setCurrent()
-      else if e.code is 'KeyL'
+      else if e.code in ['KeyL', 'ArrowRight']
         @spotify 'me/player', null, (resp) =>
           if @tracks[@current].track.id is resp.item?.id
             # currently playing, skip forward 30 seconds
@@ -76,5 +106,7 @@ export default
               method: 'put'
               data:
                 uris: [ @tracks[@current].track.uri ]
+      else if e.code in ['KeyH', 'ArrowLeft']
+        window.location.href = '/'
 
 </script>
