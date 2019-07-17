1e55d8116c1578e0dcf958ee804cbb9fbc685e4c
Alex <alex@brewingcode.net>
2019-07-17 12:31:08 -0700

    Copy files from master that DO NOT exist
    Will merge together files that exist in both this branch and in master
    in the next commit

---
+++ src/Track.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+.track(:class="{'bg-success': t.saved}")
+  div
+    a.title(target="_blank", :href="t.item.external_urls.spotify") {{t.item.name}}
+    span
+      |
+      | by
+      |
+    a.artist(target="_blank", :href="t.item.artists[0].external_urls.spotify") {{t.item.artists[0].name}}
+    span
+      |
+      | on
+      |
+    a.album(target="_blank", :href="t.item.album.external_urls.spotify") {{t.item.album.name}}
+  div(v-if="t.is_playing")
+    progress(:value="t.progress")
+</template>
+
+<script lang="coffee">
+export default
+  props: [ 't' ]
+</script>
+
+<style lang="stylus" scoped>
+.track
+  display: inline-block
+  padding: 0 1em
+  margin-bottom: 5px
+</style>
