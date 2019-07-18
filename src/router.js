ff25c31d0dc8073e1042a5c5132fadbad52b030f
Alex <alex@brewingcode.net>
2019-07-18 11:36:20 -0700

    Remove About route + view

--- src/router.js
+++ src/router.js
@@ -12,16 +12,6 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () {
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
-    },
-    {
       path: '/access_token*',
       name: 'home',
       component: Home
@@ -32,7 +22,6 @@ export default new Router({
       component: function () {
         return import(/* webpackChunkName: "playlist" */ './views/Playlist.vue')
       },
-      props: true,
     },
-  ]
+  ],
 })
