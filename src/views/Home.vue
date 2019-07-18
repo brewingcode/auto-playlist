717dbe667e41d04f7b2e915c0f7562ca69288746
Alex <alex@brewingcode.net>
2019-07-18 16:04:38 -0700

    Remove leftover CSS
    This should be (and is) in App.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -127,15 +127,6 @@ export default
 </script>
 
 <style lang="stylus">
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin: 0 auto;
-  float: none;
-
 .current
   font-size: 130%
 .playlists
